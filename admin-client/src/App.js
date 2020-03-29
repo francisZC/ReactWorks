7f670aec2d9069df48f88e7e8654bf52b7c6d3fe
francisZC <zhengchaochina@gmail.com>
2020-03-29 17:11:50 +0800

    'admin-client'

---
+++ admin-client/src/App.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * 应用跟组件
+ */
+
+import React,{ Component } from "react"
+
+ export default class App extends Component{
+    render(){
+        return <div>
+            
+        </div>
+    }
+ }
---
+++ admin-client/src/index.js
@@ -0,0 +1,9 @@
+/**入口js
+ *
+ * 
+ */
+import React,{ Component } from "react"
+import ReactDom from "react-dom"
+import App from './App'
+
+ReactDom.render(<App />, document.getElementById('root'))
